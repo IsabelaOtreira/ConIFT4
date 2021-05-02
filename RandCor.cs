f506fbdf0ada2db0ff71898b72420a8550cfbbb3
IsabelaOtreira <81163788+IsabelaOtreira@users.noreply.github.com>
2021-05-01 22:07:50 -0300

    Add files via upload
    arrumei a cor da CPU mas a da GPU ainda n funciona e quando inicia inicia branco

--- RandCor.cs
+++ RandCor.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class RandCor : MonoBehaviour
 {
     struct Cube {
-        public Vector3 posicao;
+        public Vector3 position;
         public Color color;
 
     }
 
     public ComputeShader computeShader;
-    public int interacaos = 50;
+    public int iteractions = 50;
     public int conta = 100;
     GameObject[] gameObjects;
     Cube[] data;
@@ -42,11 +42,11 @@ public class RandCor : MonoBehaviour
         {
             if (GUI.Button(new Rect(110, 0, 100, 50), "Random CPU"))
             {
-                for (int k = 0; k < interacaos; k++)
+                for (int k = 0; k < iteractions; k++)
                 {
                     for (int i = 0; i < gameObjects.Length; i++)
                     {
-                        gameObjects[i].GetComponent<MeshRenderer>().material.SetColor("_Cor", Random.ColorHSV());
+                        gameObjects[i].GetComponent<MeshRenderer>().material.SetColor("_Color", Random.ColorHSV());
                     }
 
                 }
@@ -58,18 +58,21 @@ public class RandCor : MonoBehaviour
         {
             if (GUI.Button(new Rect(220, 0, 100, 50), "Random GPU"))
             {
-                int tamanho = 4 * sizeof(float) + 3 * sizeof(float);
-                ComputeBuffer computeBuffer = new ComputeBuffer(data.Length, tamanho);
+                int size = 4 * sizeof(float) + 3 * sizeof(float);
+
+                ComputeBuffer computeBuffer = new ComputeBuffer(data.Length, size);
                 computeBuffer.SetData(data);
-                computeShader.SetBuffer(0, "cubos", computeBuffer);
-                computeShader.SetInt("interacaos", interacaos);
+
+                computeShader.SetBuffer(0, "cubes", computeBuffer);
+                computeShader.SetInt("iteraction", iteractions);
+
                 computeShader.Dispatch(0, data.Length / 10, 1, 1);
 
                 computeBuffer.GetData(data);
 
                 for (int i = 0; i < gameObjects.Length; i++)
                 {
-                    gameObjects[i].GetComponent<MeshRenderer>().material.SetColor("_Cor", data[i].color);
+                    gameObjects[i].GetComponent<MeshRenderer>().material.SetColor("_Color", data[i].color);
                 }
 
                 computeBuffer.Dispose();
@@ -93,12 +96,12 @@ public class RandCor : MonoBehaviour
                 Color _color = Random.ColorHSV();
 
                 GameObject go = GameObject.Instantiate(modelpref, new Vector3(offsetX * 0.7f, 0, offsetY * 0.7f), Quaternion.identity);
-                go.GetComponent<MeshRenderer>().material.SetColor("Cor", _color);
+                go.GetComponent<MeshRenderer>().material.SetColor("Color", _color);
 
                 gameObjects[i * conta + j] = go;
                 
                 data[i * conta + j] = new Cube();
-                data[i * conta + j].posicao = go.transform.position;
+                data[i * conta + j].position = go.transform.position;
                 data[i * conta + j].color = _color;
 
             }
